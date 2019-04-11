e67f63607b45d088cd28a2a9d73518a7e2cfc822
mohanshakshi <49307663+mohanshakshi@users.noreply.github.com>
2019-04-11 16:23:37 +0530

    4.py

---
+++ 4.py
@@ -0,0 +1,5 @@
+char=input("enter a character:)
+if(char=='A' or char=='a' or char=='E' or char=='e' or char=='I' or char=='i' or char=='O' char=='o' or char=='U' or char=='u'):
+print(char."is a vowel")
+else:
+print(char."is a consonant")
