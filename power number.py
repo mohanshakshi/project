1fc49904eea6f4709151fd823f757e616597c24e
mohanshakshi <49307663+mohanshakshi@users.noreply.github.com>
2019-05-10 14:53:59 +0530

    power.py

---
+++ power number.py
@@ -0,0 +1,17 @@
+def check_power(N,k):
+if n<= 0
+print "not a valid input"
+else:
+for i in range (1,20):
+x=k**i
+if x==n:
+print"true"
+print k,"power",i,"is",N
+break
+elif x>N:
+print "false"
+break
+check_power(2456434556,7)
+check_power(4076,87)
+check_power(76,98)
+check_power(65,7)
