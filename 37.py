426ca06689d48cab4d5ac1a905fd5c22e6987dfd
mohanshakshi <49307663+mohanshakshi@users.noreply.github.com>
2019-05-14 14:49:41 +0530

    37.py

---
+++ 37.py
@@ -0,0 +1,9 @@
+m1=input("enter the first number:")
+m2=input("enter the second number:")
+print("original value of m1:",m1)
+print("original value of m2:",m2)
+temp=n1
+n1=n2
+n2=temp
+print("swapping value of m1:",m1)
+print("swapping value of m2:",m2)
