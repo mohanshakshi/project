e85868ed0dcd7bc4b1d863c9a7a357ffc7d57781
mohanshakshi <49307663+mohanshakshi@users.noreply.github.com>
2019-05-14 11:46:11 +0530

    25.py

---
+++ 25.py
@@ -0,0 +1,14 @@
+def findMean(a,n):
+sum=0
+for i in range (o,n):
+sum+=a[i]
+return float(sum/n)
+def findMedian(a,n):
+sorded (a)
+if n%2!=0:
+return float(a[n/2])
+return float((a[int((n-1)/2)]+a[int(n/20])/2.0)
+a=[1,4,6,8,9,9,8,7,]
+n=len(a)
+print("Mean=",findmean(a,n))
+print("median=",findMedian(a,n))
