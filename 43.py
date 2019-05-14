511738caa89ff88890cded658bbaf06475638712
mohanshakshi <49307663+mohanshakshi@users.noreply.github.com>
2019-05-14 15:25:12 +0530

    43.py

---
+++ 43.py
@@ -0,0 +1,10 @@
+s1=input("enter the first string")
+s2=input("enter the string string")
+s3=s1+s2
+print("/n string after concatenation=",s3)
+print("s1=",s1)
+print("s2=",s2)
+print("s3=",s3)
+print("enter 'x' for exit')
+if s1=='x':
+exit()
